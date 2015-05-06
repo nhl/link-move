4108fd47b9a8dc62a6110a4be4b0cd96e6802638
Andrei Tomashpolskiy <sadpotato@sadpotato1.local>
2015-05-06 12:14:26 +0300

    CSV extractors

---
+++ src/main/java/com/nhl/link/etl/runtime/file/URIConnectorFactory.java
@@ -0,0 +1,36 @@
+package com.nhl.link.etl.runtime.file;
+
+import com.nhl.link.etl.EtlRuntimeException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class URIConnectorFactory implements FileConnectorFactory {
+
+    @Override
+    public FileConnector createConnector(String id) {
+
+        URI uri;
+        try {
+            // first try to get resource from classpath
+            URL url = URIConnectorFactory.class.getResource(id);
+            if (url != null) {
+                uri = url.toURI();
+            } else {
+                // probably we have absolute path to file here
+                uri = new URI(id);
+            }
+            if (!uri.isAbsolute()) {
+                throw new EtlRuntimeException(
+                        "Provided URI is not absolute (should be a classpath resource or absolute path to file)"
+                );
+            }
+        } catch (URISyntaxException e) {
+            throw new EtlRuntimeException("Failed to create connector for URI, because it is malformed: " + id, e);
+        }
+
+        return new FileConnector(uri);
+    }
+
+}
---
+++ src/main/java/com/nhl/link/etl/runtime/file/csv/CsvExtractor.java
@@ -0,0 +1,61 @@
+package com.nhl.link.etl.runtime.file.csv;
+
+import com.nhl.link.etl.EtlRuntimeException;
+import com.nhl.link.etl.RowAttribute;
+import com.nhl.link.etl.RowReader;
+import com.nhl.link.etl.extract.Extractor;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+
+public class CsvExtractor implements Extractor {
+
+    private File file;
+    private RowAttribute[] attributes;
+    private Charset charset;
+    private String delimiter;
+    private Integer readFrom;
+
+    public CsvExtractor(File file, RowAttribute[] attributes) {
+        this.file = file;
+        this.attributes = attributes;
+        this.charset = Charset.defaultCharset();
+    }
+
+    public CsvExtractor(File file, RowAttribute[] attributes, Charset charset) {
+        this(file, attributes);
+        this.charset = charset;
+    }
+
+    @Override
+    public RowReader getReader(Map<String, ?> parameters) {
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(file.toPath(), charset);
+        } catch (IOException e) {
+            throw new EtlRuntimeException("Failed to read lines from file: " + file.getPath(), e);
+        }
+
+        CsvRowReader reader = new CsvRowReader(attributes, lines);
+        if (delimiter != null) {
+            reader.setDelimiter(delimiter);
+        }
+        if (readFrom != null) {
+            reader.setReadFrom(readFrom);
+        }
+
+        return reader;
+    }
+
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public void setReadFrom(Integer readFrom) {
+        this.readFrom = readFrom;
+    }
+}
