740a89050f09e967a9bac37f67696558ff7371a3
Andrus Adamchik <andrus@objectstyle.com>
2015-05-08 13:09:32 -0400

    javadoc

--- src/main/java/com/nhl/link/etl/runtime/extract/IExtractorService.java
+++ src/main/java/com/nhl/link/etl/runtime/extract/IExtractorService.java
@@ -5,8 +5,8 @@ import com.nhl.link.etl.extract.Extractor;
 public interface IExtractorService {
 
 	/**
-	 * Returns a named {@link Extractor} or throws an exception if such
-	 * DataSource is unmapped.
+	 * Returns a named {@link Extractor} or throws an exception if such an
+	 * extractor is unmapped.
 	 */
 	Extractor getExtractor(String name);
 }
