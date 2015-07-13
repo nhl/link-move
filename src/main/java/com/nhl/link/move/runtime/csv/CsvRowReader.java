25b893471dbba3c34e1e1eed4d28e9515cd1499d
Andrei Tomashpolskiy <nordmann89@gmail.com>
2015-07-13 16:42:21 +0300

    #56 Support for non printable delimiter characters in CSV extractor

--- src/main/java/com/nhl/link/move/runtime/csv/CsvRowReader.java
+++ src/main/java/com/nhl/link/move/runtime/csv/CsvRowReader.java
@@ -28,7 +28,16 @@ public class CsvRowReader implements RowReader {
     }
 
     public void setDelimiter(String delimiter) {
-        if (delimiter.length() != 1) {
+        if (delimiter.startsWith("0x")) {
+            // we have a character code here
+            int codePoint = Integer.valueOf(delimiter.substring(2, delimiter.length()), 16);
+            if (Character.isValidCodePoint(codePoint) && Character.charCount(codePoint) == 1) {
+                this.delimiter = (char) codePoint;
+            } else {
+                throw new LmRuntimeException("Invalid delimiter (not a valid Unicode code point): "
+                        + delimiter);
+            }
+        } else if (delimiter.length() != 1) {
             throw new LmRuntimeException("Invalid delimiter (should be exactly one character): " + delimiter);
         }
         this.delimiter = delimiter.charAt(0);
