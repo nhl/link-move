10b85c44cef873d6dcf20b441065ed3806866b30
Andrus Adamchik <andrus@objectstyle.com>
2015-08-27 16:11:00 +0300

    cleanup

--- src/main/java/com/nhl/link/move/runtime/task/delete/MissingTargetsFilterStage.java
+++ src/main/java/com/nhl/link/move/runtime/task/delete/MissingTargetsFilterStage.java
@@ -1,21 +1,22 @@
 package com.nhl.link.move.runtime.task.delete;
 
-import com.nhl.link.move.Execution;
-import org.apache.cayenne.ObjectContext;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.apache.cayenne.ObjectContext;
+
+import com.nhl.link.move.Execution;
+
 /**
  * @since 1.3
  */
 public class MissingTargetsFilterStage<T> {
 
-	@SuppressWarnings("unchecked")
-	public List<T> filterMissing(Execution parentExec, ObjectContext context, Map<Object, T> mappedTargets, Set<Object> sourceKeys) {
+	public List<T> filterMissing(Execution parentExec, ObjectContext context, Map<Object, T> mappedTargets,
+			Set<Object> sourceKeys) {
 
 		List<T> matched = new ArrayList<>();
 
