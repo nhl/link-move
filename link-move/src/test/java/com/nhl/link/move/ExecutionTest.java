c69d1eb2293098cdf8008f505e46f8226f9b8c4c
Andrei Veprev <andrei.veprev@objectstyle.com>
2016-03-29 15:13:55 +0300

    Include deleted count into report
    * updated tests

--- link-move/src/test/java/com/nhl/link/move/ExecutionTest.java
+++ link-move/src/test/java/com/nhl/link/move/ExecutionTest.java
@@ -20,13 +20,13 @@ public class ExecutionTest {
 		Map<String, Object> r1 = execution.createReport();
 		assertEquals("xsync", r1.get("Task"));
 		assertEquals(5, r1.get("Parameter[a]"));
-		assertEquals(r1.toString(), 7, r1.size());
+		assertEquals(r1.toString(), 8, r1.size());
 		assertEquals("in progress", r1.get("Status"));
 
 		execution.close();
 
 		Map<String, Object> r2 = execution.createReport();
-		assertEquals(r2.toString(), 7, r2.size());
+		assertEquals(r2.toString(), 8, r2.size());
 		assertEquals("finished", r2.get("Status"));
 	}
 
