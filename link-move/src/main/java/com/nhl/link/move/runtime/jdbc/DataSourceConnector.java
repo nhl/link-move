aa8fbd9a9584e83b8f32d716be4b90992c5cd1e8
Andrus Adamchik <andrus@objectstyle.com>
2016-07-01 12:12:17 -0400

    DataSourceConnector: Out-of-the-box Java 8 support #71

--- link-move/src/main/java/com/nhl/link/move/runtime/jdbc/DataSourceConnector.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/jdbc/DataSourceConnector.java
@@ -1,11 +1,12 @@
 package com.nhl.link.move.runtime.jdbc;
 
-import javax.sql.DataSource;
-
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.configuration.server.ServerRuntime;
 import org.apache.cayenne.configuration.server.ServerRuntimeBuilder;
 import org.apache.cayenne.di.BeforeScopeEnd;
+import org.apache.cayenne.java8.CayenneJava8Module;
+
+import javax.sql.DataSource;
 
 /**
  * A {@link JdbcConnector} based on externally provided DataSource.
@@ -29,7 +30,10 @@ public class DataSourceConnector implements JdbcConnector {
 		// assigning explicit name to the Cayenne runtime to avoid transaction
 		// conflicts for similarly named DataNodes between this runtime and
 		// other Cayenne stacks present in the system.
-		this.runtime = new ServerRuntimeBuilder(name).dataSource(dataSource).build();
+		this.runtime = new ServerRuntimeBuilder(name)
+				.addModule(new CayenneJava8Module())
+				.dataSource(dataSource)
+				.build();
 		this.sharedContext = runtime.newContext();
 	}
 
