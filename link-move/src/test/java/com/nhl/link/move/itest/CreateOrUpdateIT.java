eec31521026d6ab5451af4bbd760f13e32e1d69f
Andrus Adamchik <andrus@objectstyle.com>
2017-02-26 11:36:58 +0300

    why did we have 2 identical tests ?

--- link-move/src/test/java/com/nhl/link/move/itest/CreateOrUpdateIT.java
+++ link-move/src/test/java/com/nhl/link/move/itest/CreateOrUpdateIT.java
@@ -1,55 +1,19 @@
 package com.nhl.link.move.itest;
 
-import static org.junit.Assert.assertEquals;
-
-import com.nhl.link.move.runtime.task.ITaskService;
-import com.nhl.link.move.unit.cayenne.t.Etl9t;
-import org.junit.Test;
-
+import com.nhl.link.move.Execution;
 import com.nhl.link.move.LmRuntimeException;
 import com.nhl.link.move.LmTask;
-import com.nhl.link.move.Execution;
+import com.nhl.link.move.runtime.task.ITaskService;
 import com.nhl.link.move.unit.LmIntegrationTest;
 import com.nhl.link.move.unit.cayenne.t.Etl1t;
 import com.nhl.link.move.unit.cayenne.t.Etl3t;
 import com.nhl.link.move.unit.cayenne.t.Etl5t;
+import com.nhl.link.move.unit.cayenne.t.Etl9t;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CreateOrUpdateIT extends LmIntegrationTest {
-
-	@Test
-	public void test_ByAttribute_v1() {
-
-		LmTask task = etl.service(ITaskService.class).createOrUpdate(Etl1t.class)
-				.sourceExtractor("com/nhl/link/move/itest/etl1_to_etl1t").matchBy(Etl1t.NAME).task();
-
-		srcRunSql("INSERT INTO utest.etl1 (NAME, AGE) VALUES ('a', 3)");
-		srcRunSql("INSERT INTO utest.etl1 (NAME, AGE) VALUES ('b', NULL)");
-
-		Execution e1 = task.run();
-		assertExec(2, 2, 0, 0, e1);
-		assertEquals(2, targetScalar("SELECT count(1) from utest.etl1t"));
-		assertEquals(1, targetScalar("SELECT count(1) from utest.etl1t WHERE NAME = 'a' AND age = 3"));
-		assertEquals(1, targetScalar("SELECT count(1) from utest.etl1t WHERE NAME = 'b' AND age is null"));
-
-		srcRunSql("INSERT INTO utest.etl1 (NAME) VALUES ('c')");
-		srcRunSql("UPDATE utest.etl1 SET AGE = 5 WHERE NAME = 'a'");
-
-		Execution e2 = task.run();
-		assertExec(3, 1, 1, 0, e2);
-		assertEquals(3, targetScalar("SELECT count(1) from utest.etl1t"));
-		assertEquals(1, targetScalar("SELECT count(1) from utest.etl1t WHERE NAME = 'a' AND age = 5"));
-		assertEquals(1, targetScalar("SELECT count(1) from utest.etl1t WHERE NAME = 'c' AND age is null"));
-
-		srcRunSql("DELETE FROM utest.etl1 WHERE NAME = 'a'");
-
-		Execution e3 = task.run();
-		assertExec(2, 0, 0, 0, e3);
-		assertEquals(3, targetScalar("SELECT count(1) from utest.etl1t"));
-		assertEquals(1, targetScalar("SELECT count(1) from utest.etl1t WHERE NAME = 'a' AND age = 5"));
-
-		Execution e4 = task.run();
-		assertExec(2, 0, 0, 0, e4);
-	}
 
 	@Test
 	public void test_ByAttribute() {
