9651150cefcf5a8b8a8cc23791c0a18b0aa23285
Andrus Adamchik <andrus@objectstyle.com>
2017-04-13 07:56:41 -0700

    javadocs - we have more normalizers now. becoming immpractical to list them all here

--- link-move/src/main/java/com/nhl/link/move/runtime/jdbc/JdbcNormalizer.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/jdbc/JdbcNormalizer.java
@@ -3,61 +3,62 @@ package com.nhl.link.move.runtime.jdbc;
 import org.apache.cayenne.map.DbAttribute;
 
 /**
- * Instances of this class are used by LM matching runtime to convert extracted
- * sources (on per-attribute basis) into a form that is comparable with target
- * entries. Each instance of this class should handle one JDBC type. By default,
- * LM ships with the following normalizers: {@link BigIntNormalizer},
- * {@link IntegerNormalizer}, {@link DecimalNormalizer},
- * {@link BooleanNormalizer}
+ * Used by LM matching runtime to convert extracted sources (on per-attribute basis) into a form that is compatible
+ * with target entries. Each instance of this class should handle one JDBC type.
+ *
+ * @see {@link BigIntNormalizer}
+ * @see {@link IntegerNormalizer}
+ * @see {@link DecimalNormalizer}
+ * @see {@link BooleanNormalizer}
  */
 public abstract class JdbcNormalizer<T> {
 
-	private final Class<T> type;
+    private final Class<T> type;
 
-	public JdbcNormalizer(Class<T> javaType) {
-		this.type = javaType;
-	}
+    public JdbcNormalizer(Class<T> javaType) {
+        this.type = javaType;
+    }
 
-	/**
-	 * @return JDBC type that this normalizer works with.
-	 * @see java.sql.Types
-	 */
-	public Class<T> getType() {
-		return type;
-	}
-
-	public String getTypeName() {
-		return type.getName();
-	}
-
-	/**
-	 * @since 1.7
-	 */
-	@SuppressWarnings("unchecked")
-	public T normalize(Object value, DbAttribute targetAttribute) {
-
-		T result;
-		if (value == null) {
-			result = null;
-		} else if (type.isAssignableFrom(value.getClass())) {
-			result = (T) value;
-		} else {
-			result = doNormalize(value, targetAttribute);
-		}
-
-		return postNormalize(result, targetAttribute);
-	}
-
-	protected abstract T doNormalize(Object value, DbAttribute targetAttribute);
-
-	/**
-	 * Override this method to do post-processing of the normalized value
-	 * (e.g. additional scaling of a decimal)
-	 *
-	 * @see DecimalNormalizer
+    /**
+     * @return JDBC type that this normalizer works with.
+     * @see java.sql.Types
      */
-	protected T postNormalize(T normalized, DbAttribute targetAttribute) {
-		// by default just return the value
-		return normalized;
-	}
+    public Class<T> getType() {
+        return type;
+    }
+
+    public String getTypeName() {
+        return type.getName();
+    }
+
+    /**
+     * @since 1.7
+     */
+    @SuppressWarnings("unchecked")
+    public T normalize(Object value, DbAttribute targetAttribute) {
+
+        T result;
+        if (value == null) {
+            result = null;
+        } else if (type.isAssignableFrom(value.getClass())) {
+            result = (T) value;
+        } else {
+            result = doNormalize(value, targetAttribute);
+        }
+
+        return postNormalize(result, targetAttribute);
+    }
+
+    protected abstract T doNormalize(Object value, DbAttribute targetAttribute);
+
+    /**
+     * Override this method to do post-processing of the normalized value
+     * (e.g. additional scaling of a decimal)
+     *
+     * @see DecimalNormalizer
+     */
+    protected T postNormalize(T normalized, DbAttribute targetAttribute) {
+        // by default just return the value
+        return normalized;
+    }
 }
