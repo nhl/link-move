b738f53b6bff32caa934f20b4319ad1c25e5f61e
Andrus Adamchik <andrus@objectstyle.com>
2017-09-25 15:05:59 +0300

    Refactoring JdbcNormalizer to ValueConverter #135
    * partially restoring backwards compatible API

--- link-move/src/main/java/com/nhl/link/move/runtime/LmRuntimeBuilder.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/LmRuntimeBuilder.java
@@ -18,6 +18,7 @@ import com.nhl.link.move.runtime.extractor.model.IExtractorModelLoader;
 import com.nhl.link.move.runtime.extractor.model.IExtractorModelService;
 import com.nhl.link.move.runtime.jdbc.JdbcConnector;
 import com.nhl.link.move.runtime.jdbc.JdbcExtractorFactory;
+import com.nhl.link.move.runtime.jdbc.JdbcNormalizer;
 import com.nhl.link.move.runtime.key.IKeyAdapterFactory;
 import com.nhl.link.move.runtime.key.KeyAdapterFactory;
 import com.nhl.link.move.runtime.path.IPathNormalizer;
@@ -39,12 +40,14 @@ import com.nhl.link.move.valueconverter.ValueConverterFactory;
 import com.nhl.link.move.writer.ITargetPropertyWriterService;
 import com.nhl.link.move.writer.TargetPropertyWriterService;
 import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.apache.cayenne.dba.TypesMapping;
 import org.apache.cayenne.di.Binder;
 import org.apache.cayenne.di.DIBootstrap;
 import org.apache.cayenne.di.Injector;
 import org.apache.cayenne.di.Key;
 import org.apache.cayenne.di.MapBuilder;
 import org.apache.cayenne.di.Module;
+import org.apache.cayenne.map.DbAttribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -65,181 +68,192 @@ import java.util.ServiceLoader;
  */
 public class LmRuntimeBuilder {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(LmRuntimeBuilder.class);
-
-	/**
-	 * A DI property that defines the root directory to resolve locations of
-	 * extractor config files.
-	 * 
-	 * @since 1.4
-	 */
-	public static final String FILE_EXTRACTOR_MODEL_ROOT_DIR = "com.nhl.link.move.extrator.root.dir";
-
-	public static final String START_TOKEN_VAR = "startToken";
-	public static final String END_TOKEN_VAR = "endToken";
-
-	private Map<String, Connector> connectors;
-	private Map<String, IConnectorFactory> connectorFactories;
-	private Map<String, Class<? extends IConnectorFactory<? extends Connector>>> connectorFactoryTypes;
-
-	private Map<String, IExtractorFactory> extractorFactories;
-	private Map<String, Class<? extends IExtractorFactory<?>>> extractorFactoryTypes;
-
-	private Map<String, ValueConverter> valueConverters;
-
-	private IExtractorModelLoader extractorModelLoader;
-	private File extractorModelsRoot;
-
-	private ITokenManager tokenManager;
-	private ServerRuntime targetRuntime;
-	private Collection<LinkEtlAdapter> adapters;
-
-	public LmRuntimeBuilder() {
-		this.connectors = new HashMap<>();
-		this.connectorFactories = new HashMap<>();
-		this.connectorFactoryTypes = new HashMap<>();
-		this.extractorFactories = new HashMap<>();
-		this.extractorFactoryTypes = new HashMap<>();
-		this.valueConverters = new HashMap<>();
-		this.adapters = new ArrayList<>();
-
-		// default normalizers
-		valueConverters.put(Boolean.class.getName(), BooleanConverter.getConverter());
-		valueConverters.put(Boolean.TYPE.getName(), BooleanConverter.getConverter());
-		valueConverters.put(Long.class.getName(), LongConverter.getConverter());
-		valueConverters.put(Long.TYPE.getName(), LongConverter.getConverter());
-		valueConverters.put(Integer.class.getName(), IntegerConverter.getConverter());
-		valueConverters.put(Integer.TYPE.getName(), IntegerConverter.getConverter());
-		valueConverters.put(BigDecimal.class.getName(), new BigDecimalConverter());
-		valueConverters.put(LocalDate.class.getName(), new LocalDateConverter());
-		valueConverters.put(LocalTime.class.getName(), new LocalTimeConverter());
-		valueConverters.put(LocalDateTime.class.getName(), new LocalDateTimeConverter());
-		valueConverters.put(String.class.getName(), new StringConverter());
-	}
-
-	/**
-	 * Adds an adapter that can override existing DI services or add new ones.
-	 *
-	 * @since 1.1
-	 */
-	public LmRuntimeBuilder adapter(LinkEtlAdapter adapter) {
-		this.adapters.add(adapter);
-		return this;
-	}
-
-	/**
-	 * Sets a target Cayenne runtime for this ETL stack.
-	 */
-	public LmRuntimeBuilder withTargetRuntime(ServerRuntime targetRuntime) {
-		this.targetRuntime = targetRuntime;
-		return this;
-	}
-
-	public LmRuntimeBuilder withTokenManager(ITokenManager tokenManager) {
-		this.tokenManager = tokenManager;
-		return this;
-	}
-
-	public LmRuntimeBuilder withConnector(String id, Connector connector) {
-		connectors.put(id, connector);
-		return this;
-	}
-
-	public <C extends Connector> LmRuntimeBuilder withConnectorFactory(Class<C> connectorType,
-			IConnectorFactory<C> factory) {
-		connectorFactories.put(connectorType.getName(), factory);
-		return this;
-	}
-
-	public <C extends Connector> LmRuntimeBuilder withConnectorFactory(Class<C> connectorType,
-			Class<? extends IConnectorFactory<C>> factoryType) {
-		connectorFactoryTypes.put(connectorType.getName(), factoryType);
-		return this;
-	}
-
-	/**
-	 * Ensures source JDBC connectors are created for each one of the target
-	 * DataSources. Connector ID will be equal to the DataNode name of the
-	 * target.
-	 * <p>
-	 * This configuration conveniently reuses already pre-configured target
-	 * DataSources. It may be useful in cases when the data is transferred
-	 * between the tables on the same DB server, or if connectors are known in
-	 * advance and can be modeled in Cayenne as DataNodes.
-	 *
-	 * @since 1.1
-	 */
-	public LmRuntimeBuilder withConnectorFromTarget() {
-		connectorFactoryTypes.put(JdbcConnector.class.getName(), TargetConnectorFactory.class);
-		return this;
-	}
-
-	/**
-	 * Adds an extra factory to the map of extractor factories. Note that
-	 * {@link JdbcExtractorFactory} is loaded by default and does not have to be
-	 * configured explicitly.
-	 */
-	public LmRuntimeBuilder withExtractorFactory(String extractorType, Class<? extends IExtractorFactory<?>> factoryType) {
-		extractorFactoryTypes.put(extractorType, factoryType);
-		return this;
-	}
-
-	/**
-	 * Adds an extra factory to the map of extractor factories. Note that
-	 * {@link JdbcExtractorFactory} is loaded by default and does not have to be
-	 * configured explicitly.
-	 */
-	public LmRuntimeBuilder withExtractorFactory(String extractorType, IExtractorFactory<?> factory) {
-		extractorFactories.put(extractorType, factory);
-		return this;
-	}
-
-	/**
-	 * @since 2.4
+    /**
+     * A DI property that defines the root directory to resolve locations of
+     * extractor config files.
+     *
+     * @since 1.4
      */
-	public LmRuntimeBuilder valueConverter(Class<?> javaType, ValueConverter converter) {
-		valueConverters.put(javaType.getName(), converter);
-		return this;
-	}
+    public static final String FILE_EXTRACTOR_MODEL_ROOT_DIR = "com.nhl.link.move.extrator.root.dir";
+    public static final String START_TOKEN_VAR = "startToken";
+    public static final String END_TOKEN_VAR = "endToken";
+    private static final Logger LOGGER = LoggerFactory.getLogger(LmRuntimeBuilder.class);
+    private Map<String, Connector> connectors;
+    private Map<String, IConnectorFactory> connectorFactories;
+    private Map<String, Class<? extends IConnectorFactory<? extends Connector>>> connectorFactoryTypes;
 
-	/**
-	 * @since 1.4
-	 */
-	public LmRuntimeBuilder extractorModelLoader(IExtractorModelLoader extractorModelLoader) {
-		this.extractorModelLoader = extractorModelLoader;
-		this.extractorModelsRoot = null;
-		return this;
-	}
+    private Map<String, IExtractorFactory> extractorFactories;
+    private Map<String, Class<? extends IExtractorFactory<?>>> extractorFactoryTypes;
 
-	/**
-	 * @since 1.4
-	 */
-	public LmRuntimeBuilder extractorModelsRoot(File rootDir) {
-		this.extractorModelLoader = null;
-		this.extractorModelsRoot = rootDir;
-		return this;
-	}
+    private Map<String, ValueConverter> valueConverters;
 
-	/**
-	 * @since 1.4
-	 */
-	public LmRuntimeBuilder extractorModelsRoot(String rootDirPath) {
-		this.extractorModelLoader = null;
-		this.extractorModelsRoot = new File(rootDirPath);
-		return this;
-	}
+    private IExtractorModelLoader extractorModelLoader;
+    private File extractorModelsRoot;
 
-	public LmRuntime build() throws IllegalStateException {
+    private ITokenManager tokenManager;
+    private ServerRuntime targetRuntime;
+    private Collection<LinkEtlAdapter> adapters;
 
-		if (targetRuntime == null) {
-			throw new IllegalStateException("Required Cayenne 'targetRuntime' is not set");
-		}
+    public LmRuntimeBuilder() {
+        this.connectors = new HashMap<>();
+        this.connectorFactories = new HashMap<>();
+        this.connectorFactoryTypes = new HashMap<>();
+        this.extractorFactories = new HashMap<>();
+        this.extractorFactoryTypes = new HashMap<>();
+        this.valueConverters = new HashMap<>();
+        this.adapters = new ArrayList<>();
 
-		if (tokenManager == null) {
-			tokenManager = new InMemoryTokenManager();
-		}
+        // default normalizers
+        valueConverters.put(Boolean.class.getName(), BooleanConverter.getConverter());
+        valueConverters.put(Boolean.TYPE.getName(), BooleanConverter.getConverter());
+        valueConverters.put(Long.class.getName(), LongConverter.getConverter());
+        valueConverters.put(Long.TYPE.getName(), LongConverter.getConverter());
+        valueConverters.put(Integer.class.getName(), IntegerConverter.getConverter());
+        valueConverters.put(Integer.TYPE.getName(), IntegerConverter.getConverter());
+        valueConverters.put(BigDecimal.class.getName(), new BigDecimalConverter());
+        valueConverters.put(LocalDate.class.getName(), new LocalDateConverter());
+        valueConverters.put(LocalTime.class.getName(), new LocalTimeConverter());
+        valueConverters.put(LocalDateTime.class.getName(), new LocalDateTimeConverter());
+        valueConverters.put(String.class.getName(), new StringConverter());
+    }
 
-		Module etlModule = binder -> {
+    /**
+     * Adds an adapter that can override existing DI services or add new ones.
+     *
+     * @since 1.1
+     */
+    public LmRuntimeBuilder adapter(LinkEtlAdapter adapter) {
+        this.adapters.add(adapter);
+        return this;
+    }
+
+    /**
+     * Sets a target Cayenne runtime for this ETL stack.
+     */
+    public LmRuntimeBuilder withTargetRuntime(ServerRuntime targetRuntime) {
+        this.targetRuntime = targetRuntime;
+        return this;
+    }
+
+    public LmRuntimeBuilder withTokenManager(ITokenManager tokenManager) {
+        this.tokenManager = tokenManager;
+        return this;
+    }
+
+    public LmRuntimeBuilder withConnector(String id, Connector connector) {
+        connectors.put(id, connector);
+        return this;
+    }
+
+    public <C extends Connector> LmRuntimeBuilder withConnectorFactory(Class<C> connectorType,
+                                                                       IConnectorFactory<C> factory) {
+        connectorFactories.put(connectorType.getName(), factory);
+        return this;
+    }
+
+    public <C extends Connector> LmRuntimeBuilder withConnectorFactory(Class<C> connectorType,
+                                                                       Class<? extends IConnectorFactory<C>> factoryType) {
+        connectorFactoryTypes.put(connectorType.getName(), factoryType);
+        return this;
+    }
+
+    /**
+     * Ensures source JDBC connectors are created for each one of the target
+     * DataSources. Connector ID will be equal to the DataNode name of the
+     * target.
+     * <p>
+     * This configuration conveniently reuses already pre-configured target
+     * DataSources. It may be useful in cases when the data is transferred
+     * between the tables on the same DB server, or if connectors are known in
+     * advance and can be modeled in Cayenne as DataNodes.
+     *
+     * @since 1.1
+     */
+    public LmRuntimeBuilder withConnectorFromTarget() {
+        connectorFactoryTypes.put(JdbcConnector.class.getName(), TargetConnectorFactory.class);
+        return this;
+    }
+
+    /**
+     * Adds an extra factory to the map of extractor factories. Note that
+     * {@link JdbcExtractorFactory} is loaded by default and does not have to be
+     * configured explicitly.
+     */
+    public LmRuntimeBuilder withExtractorFactory(String extractorType, Class<? extends IExtractorFactory<?>> factoryType) {
+        extractorFactoryTypes.put(extractorType, factoryType);
+        return this;
+    }
+
+    /**
+     * Adds an extra factory to the map of extractor factories. Note that
+     * {@link JdbcExtractorFactory} is loaded by default and does not have to be
+     * configured explicitly.
+     */
+    public LmRuntimeBuilder withExtractorFactory(String extractorType, IExtractorFactory<?> factory) {
+        extractorFactories.put(extractorType, factory);
+        return this;
+    }
+
+    /**
+     * @since 2.4
+     */
+    public LmRuntimeBuilder valueConverter(Class<?> javaType, ValueConverter converter) {
+        valueConverters.put(javaType.getName(), converter);
+        return this;
+    }
+
+    /**
+     * @since 1.7
+     * @deprecated since 2.4 in favor of {@link #valueConverter(Class, ValueConverter)}.
+     */
+    @Deprecated
+    public LmRuntimeBuilder withJdbcNormalizer(Class<?> javaType, JdbcNormalizer<?> normalizer) {
+        return valueConverter(javaType, (v, s) -> {
+            DbAttribute placeholder = new DbAttribute("_placeholder");
+            placeholder.setScale(s);
+            placeholder.setType(TypesMapping.getSqlTypeByJava(javaType));
+            return normalizer.normalize(v, placeholder);
+        });
+    }
+
+    /**
+     * @since 1.4
+     */
+    public LmRuntimeBuilder extractorModelLoader(IExtractorModelLoader extractorModelLoader) {
+        this.extractorModelLoader = extractorModelLoader;
+        this.extractorModelsRoot = null;
+        return this;
+    }
+
+    /**
+     * @since 1.4
+     */
+    public LmRuntimeBuilder extractorModelsRoot(File rootDir) {
+        this.extractorModelLoader = null;
+        this.extractorModelsRoot = rootDir;
+        return this;
+    }
+
+    /**
+     * @since 1.4
+     */
+    public LmRuntimeBuilder extractorModelsRoot(String rootDirPath) {
+        this.extractorModelLoader = null;
+        this.extractorModelsRoot = new File(rootDirPath);
+        return this;
+    }
+
+    public LmRuntime build() throws IllegalStateException {
+
+        if (targetRuntime == null) {
+            throw new IllegalStateException("Required Cayenne 'targetRuntime' is not set");
+        }
+
+        if (tokenManager == null) {
+            tokenManager = new InMemoryTokenManager();
+        }
+
+        Module etlModule = binder -> {
 
             bindModelLoader(binder);
 
@@ -305,36 +319,36 @@ public class LmRuntimeBuilder {
             }
         };
 
-		final Injector injector = DIBootstrap.createInjector(etlModule);
+        final Injector injector = DIBootstrap.createInjector(etlModule);
 
-		return new LmRuntime() {
+        return new LmRuntime() {
 
-			@Override
-			public <T> T service(Class<T> serviceType) {
-				return injector.getInstance(serviceType);
-			}
+            @Override
+            public <T> T service(Class<T> serviceType) {
+                return injector.getInstance(serviceType);
+            }
 
-			@Override
-			public void shutdown() {
-				injector.shutdown();
-			}
-		};
-	}
+            @Override
+            public void shutdown() {
+                injector.shutdown();
+            }
+        };
+    }
 
-	void bindModelLoader(Binder binder) {
-		if (extractorModelLoader != null) {
-			binder.bind(IExtractorModelLoader.class).toInstance(extractorModelLoader);
-		} else if (extractorModelsRoot != null) {
+    void bindModelLoader(Binder binder) {
+        if (extractorModelLoader != null) {
+            binder.bind(IExtractorModelLoader.class).toInstance(extractorModelLoader);
+        } else if (extractorModelsRoot != null) {
 
-			if (!extractorModelsRoot.isDirectory()) {
-				LOGGER.warn("Extractor models root is not a valid directory: " + extractorModelsRoot);
-			}
+            if (!extractorModelsRoot.isDirectory()) {
+                LOGGER.warn("Extractor models root is not a valid directory: " + extractorModelsRoot);
+            }
 
-			binder.bind(IExtractorModelLoader.class).to(FileExtractorModelLoader.class);
-			binder.bind(Key.get(File.class, FILE_EXTRACTOR_MODEL_ROOT_DIR)).toInstance(extractorModelsRoot);
-		} else {
-			binder.bind(IExtractorModelLoader.class).to(ClasspathExtractorModelLoader.class);
-		}
-	}
+            binder.bind(IExtractorModelLoader.class).to(FileExtractorModelLoader.class);
+            binder.bind(Key.get(File.class, FILE_EXTRACTOR_MODEL_ROOT_DIR)).toInstance(extractorModelsRoot);
+        } else {
+            binder.bind(IExtractorModelLoader.class).to(ClasspathExtractorModelLoader.class);
+        }
+    }
 
 }
