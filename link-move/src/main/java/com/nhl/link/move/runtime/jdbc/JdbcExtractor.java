9bd33b5d1e0d27f0c18ac6c4015244eebfa17d56
Andrus Adamchik <andrus@objectstyle.com>
2018-09-24 15:33:36 -0400

    no changes, cleanup, formatting

--- link-move/src/main/java/com/nhl/link/move/runtime/jdbc/JdbcExtractor.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/jdbc/JdbcExtractor.java
@@ -1,54 +1,52 @@
 package com.nhl.link.move.runtime.jdbc;
 
-import java.util.Map;
-
+import com.nhl.link.move.RowAttribute;
+import com.nhl.link.move.RowReader;
+import com.nhl.link.move.extractor.Extractor;
 import org.apache.cayenne.DataRow;
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.query.CapsStrategy;
 import org.apache.cayenne.query.SQLSelect;
 
-import com.nhl.link.move.RowAttribute;
-import com.nhl.link.move.RowReader;
-import com.nhl.link.move.extractor.Extractor;
+import java.util.Map;
 
 public class JdbcExtractor implements Extractor {
 
-	private ObjectContext context;
-	private String sqlTemplate;
-	private CapsStrategy capsStrategy;
-	private RowAttribute[] attributes;
+    private ObjectContext context;
+    private String sqlTemplate;
+    private CapsStrategy capsStrategy;
+    private RowAttribute[] attributes;
 
-	public JdbcExtractor(ObjectContext context, RowAttribute[] attributes, String sqlTemplate,
-			CapsStrategy capsStrategy) {
-		this.context = context;
-		this.sqlTemplate = sqlTemplate;
-		this.capsStrategy = capsStrategy;
-		this.attributes = attributes;
-	}
+    public JdbcExtractor(ObjectContext context, RowAttribute[] attributes, String sqlTemplate,
+                         CapsStrategy capsStrategy) {
+        this.context = context;
+        this.sqlTemplate = sqlTemplate;
+        this.capsStrategy = capsStrategy;
+        this.attributes = attributes;
+    }
 
-	@Override
-	public RowReader getReader(Map<String, ?> parameters) {
+    @Override
+    public RowReader getReader(Map<String, ?> parameters) {
 
-		// TODO: fetching DataRows and then converting them to Object[] is kind
-		// of expensive... maybe we can create Object[] bypassing DR, ideally by
-		// iterating a JDBC ResultSet
+        // TODO: fetching DataRows and then converting them to Object[] is kind of expensive...
+        // maybe we can create Object[] bypassing DR, ideally by iterating a JDBC ResultSet
 
-		SQLSelect<DataRow> select = SQLSelect.dataRowQuery(sqlTemplate);
-		select.params(parameters);
+        SQLSelect<DataRow> select = SQLSelect
+                .dataRowQuery(sqlTemplate)
+                .params(parameters);
 
-		switch (capsStrategy) {
+        switch (capsStrategy) {
+            case LOWER:
+                select.lowerColumnNames();
+                break;
+            case UPPER:
+                select.upperColumnNames();
+                break;
+            case DEFAULT:
+                select.upperColumnNames();
+                break;
+        }
 
-		case LOWER:
-			select.lowerColumnNames();
-			break;
-		case UPPER:
-			select.upperColumnNames();
-			break;
-		case DEFAULT:
-			select.upperColumnNames();
-			break;
-		}
-
-		return new JdbcRowReader(attributes, context.iterator(select));
-	}
+        return new JdbcRowReader(attributes, context.iterator(select));
+    }
 }
--- link-move/src/main/java/com/nhl/link/move/runtime/jdbc/JdbcRowReader.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/jdbc/JdbcRowReader.java
@@ -1,91 +1,89 @@
 package com.nhl.link.move.runtime.jdbc;
 
+import com.nhl.link.move.BaseRowAttribute;
+import com.nhl.link.move.Row;
+import com.nhl.link.move.RowAttribute;
+import com.nhl.link.move.RowReader;
+import org.apache.cayenne.DataRow;
+import org.apache.cayenne.ResultIterator;
+import org.apache.cayenne.exp.parser.ASTDbPath;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import com.nhl.link.move.BaseRowAttribute;
-import org.apache.cayenne.DataRow;
-import org.apache.cayenne.ResultIterator;
-import org.apache.cayenne.exp.parser.ASTDbPath;
-
-import com.nhl.link.move.Row;
-import com.nhl.link.move.RowAttribute;
-import com.nhl.link.move.RowReader;
-
 public class JdbcRowReader implements RowReader {
 
-	private ResultIterator<DataRow> rows;
-	private RowAttribute[] attributes;
+    private ResultIterator<DataRow> rows;
+    private RowAttribute[] attributes;
 
-	public JdbcRowReader(RowAttribute[] attributes, ResultIterator<DataRow> rows) {
-		this.rows = rows;
-		this.attributes = attributes;
-	}
+    public JdbcRowReader(RowAttribute[] attributes, ResultIterator<DataRow> rows) {
+        this.rows = rows;
+        this.attributes = attributes;
+    }
 
-	@Override
-	public Iterator<Row> iterator() {
-		final Iterator<DataRow> drIt = rows.iterator();
+    @Override
+    public Iterator<Row> iterator() {
+        Iterator<DataRow> drIt = rows.iterator();
 
-		return new Iterator<Row>() {
+        return new Iterator<Row>() {
 
-			// compiled dynamically unless explicitly set for RowReader...
-			private RowAttribute[] itAttributes;
+            // compiled dynamically unless explicitly set for RowReader...
+            private RowAttribute[] itAttributes;
 
-			{
-				itAttributes = attributes;
-			}
+            {
+                itAttributes = attributes;
+            }
 
-			// if not set, compiles attributes dynamically from a DataRow and
-			// caches them in iterator
-			private RowAttribute[] attributes(DataRow row) {
+            // if not set, compiles attributes dynamically from a DataRow and caches them in iterator
+            private RowAttribute[] attributes(DataRow row) {
 
-				if (itAttributes == null) {
-					itAttributes = attributesFromDataRow(row);
-				}
+                if (itAttributes == null) {
+                    itAttributes = attributesFromDataRow(row);
+                }
 
-				return itAttributes;
-			}
+                return itAttributes;
+            }
 
-			@Override
-			public boolean hasNext() {
-				return drIt.hasNext();
-			}
+            @Override
+            public boolean hasNext() {
+                return drIt.hasNext();
+            }
 
-			@Override
-			public Row next() {
+            @Override
+            public Row next() {
 
-				DataRow row = drIt.next();
-				RowAttribute[] attributes = attributes(row);
-				return new DataRowRow(attributes, row);
-			}
+                DataRow row = drIt.next();
+                RowAttribute[] attributes = attributes(row);
+                return new DataRowRow(attributes, row);
+            }
 
-			@Override
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
-		};
-	}
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
 
-	RowAttribute[] attributesFromDataRow(DataRow row) {
+    RowAttribute[] attributesFromDataRow(DataRow row) {
 
-		List<String> names = new ArrayList<>(row.keySet());
+        List<String> names = new ArrayList<>(row.keySet());
 
-		// ensure predictable order on each run...
-		Collections.sort(names);
+        // ensure predictable order on each run...
+        Collections.sort(names);
 
-		RowAttribute[] attributes = new RowAttribute[row.size()];
-		for (int i = 0; i < attributes.length; i++) {
-			String name = names.get(i);
-			attributes[i] = new BaseRowAttribute(Object.class, name, ASTDbPath.DB_PREFIX + name, i);
-		}
+        RowAttribute[] attributes = new RowAttribute[row.size()];
+        for (int i = 0; i < attributes.length; i++) {
+            String name = names.get(i);
+            attributes[i] = new BaseRowAttribute(Object.class, name, ASTDbPath.DB_PREFIX + name, i);
+        }
 
-		return attributes;
-	}
+        return attributes;
+    }
 
-	@Override
-	public void close() {
-		rows.close();
-	}
+    @Override
+    public void close() {
+        rows.close();
+    }
 }
