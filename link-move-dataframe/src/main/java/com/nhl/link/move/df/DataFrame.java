b0f03731ffc72a260fbd242d5ebf1af340298f95
Andrus Adamchik <andrus@objectstyle.com>
2018-12-31 12:48:01 +0900

    DataFrame based micro-batches #161
    * perhaps batching should be done outside the DataFrame API.. DataFrame is itself a "micro-batch"

--- link-move-dataframe/src/main/java/com/nhl/link/move/df/DataFrame.java
+++ link-move-dataframe/src/main/java/com/nhl/link/move/df/DataFrame.java
@@ -10,12 +10,9 @@ import com.nhl.link.move.df.map.DataRowMapper;
 import com.nhl.link.move.df.map.ValueMapper;
 import com.nhl.link.move.df.zip.Zipper;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 
 /**
  * Represents immutable 2D data.
@@ -95,29 +92,4 @@ public interface DataFrame extends Iterable<DataRow> {
 
     @Override
     Iterator<DataRow> iterator();
-
-    default void consumeAsBatches(Consumer<DataFrame> consumer, int batchSize) {
-
-        if (batchSize <= 0) {
-            throw new IllegalArgumentException("'batchSize' must be positive: " + batchSize);
-        }
-
-        Index columns = getColumns();
-        List<DataRow> rows = new ArrayList<>(batchSize);
-
-        for (DataRow dr : this) {
-
-            rows.add(dr);
-
-            if (rows.size() % batchSize == 0) {
-                consumer.accept(new TransformingDataFrame(columns, rows));
-                rows = new ArrayList<>(batchSize);
-            }
-        }
-
-        // consume leftovers
-        if (!rows.isEmpty()) {
-            consumer.accept(new TransformingDataFrame(columns, rows));
-        }
-    }
 }
--- link-move-dataframe/src/test/java/com/nhl/link/move/df/DataFrameTest.java
+++ link-move-dataframe/src/test/java/com/nhl/link/move/df/DataFrameTest.java
@@ -2,24 +2,9 @@ package com.nhl.link.move.df;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static java.util.Arrays.asList;
-import static org.junit.Assert.*;
 
 public class DataFrameTest {
-
-    private DataFrame createDataFrame() {
-        Index columns = new Index("a");
-        List<DataRow> rows = asList(
-                new ArrayDataRow(columns, "one"),
-                new ArrayDataRow(columns, "two"),
-                new ArrayDataRow(columns, "three"),
-                new ArrayDataRow(columns, "four"));
-
-
-        return new SimpleDataFrame(columns, rows);
-    }
 
     @Test
     public void testFromRows() {
@@ -47,65 +32,5 @@ public class DataFrameTest {
                 .assertLength(2)
                 .assertRow(0, 1)
                 .assertRow(1, 2);
-    }
-
-    @Test
-    public void testConsumeAsBatches_SizeNotDivisible() {
-
-        int[] batchCounter = new int[1];
-        int[] elementCounter = new int[1];
-
-        createDataFrame().consumeAsBatches(f -> {
-            batchCounter[0] += 1;
-            f.forEach(r -> elementCounter[0] += 1);
-        }, 3);
-
-        assertEquals(2, batchCounter[0]);
-        assertEquals(4, elementCounter[0]);
-    }
-
-    @Test
-    public void testConsumeAsBatches_SizeDivisible() {
-
-        int[] batchCounter = new int[1];
-        int[] elementCounter = new int[1];
-
-        createDataFrame().consumeAsBatches(f -> {
-            batchCounter[0] += 1;
-            f.forEach(r -> elementCounter[0] += 1);
-        }, 2);
-
-        assertEquals(2, batchCounter[0]);
-        assertEquals(4, elementCounter[0]);
-    }
-
-    @Test
-    public void testConsumeAsBatches_SizeSame() {
-
-        int[] batchCounter = new int[1];
-        int[] elementCounter = new int[1];
-
-        createDataFrame().consumeAsBatches(f -> {
-            batchCounter[0] += 1;
-            f.forEach(r -> elementCounter[0] += 1);
-        }, 4);
-
-        assertEquals(1, batchCounter[0]);
-        assertEquals(4, elementCounter[0]);
-    }
-
-    @Test
-    public void testConsumeAsBatches_SizeGreater() {
-
-        int[] batchCounter = new int[1];
-        int[] elementCounter = new int[1];
-
-        createDataFrame().consumeAsBatches(f -> {
-            batchCounter[0] += 1;
-            f.forEach(r -> elementCounter[0] += 1);
-        }, 1000);
-
-        assertEquals(1, batchCounter[0]);
-        assertEquals(4, elementCounter[0]);
     }
 }
