4ac2c80b51fc0b6d7fa5f659e142271b71904981
Andrus Adamchik <andrus@objectstyle.com>
2019-01-01 16:20:22 +0900

    refactoring - initialization of row header prior to RowReader creation

--- link-move/src/main/java/com/nhl/link/move/runtime/jdbc/JdbcRowReader.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/jdbc/JdbcRowReader.java
@@ -1,62 +1,37 @@
 package com.nhl.link.move.runtime.jdbc;
 
-import com.nhl.link.move.BaseRowAttribute;
 import com.nhl.link.move.Row;
 import com.nhl.link.move.RowAttribute;
 import com.nhl.link.move.RowReader;
 import org.apache.cayenne.DataRow;
-import org.apache.cayenne.ResultIterator;
-import org.apache.cayenne.exp.parser.ASTDbPath;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Objects;
 
 public class JdbcRowReader implements RowReader {
 
-    private ResultIterator<DataRow> rows;
-    private RowAttribute[] attributes;
+    private DataRowIterator rows;
+    private RowAttribute[] rowHeader;
 
-    public JdbcRowReader(RowAttribute[] attributes, ResultIterator<DataRow> rows) {
-        this.rows = rows;
-        this.attributes = attributes;
+    public JdbcRowReader(RowAttribute[] rowHeader, DataRowIterator rows) {
+        this.rows = Objects.requireNonNull(rows);
+        this.rowHeader = Objects.requireNonNull(rowHeader);
     }
 
     @Override
     public Iterator<Row> iterator() {
-        Iterator<DataRow> drIt = rows.iterator();
 
         return new Iterator<Row>() {
 
-            // compiled dynamically unless explicitly set for RowReader...
-            private RowAttribute[] itAttributes;
-
-            {
-                itAttributes = attributes;
-            }
-
-            // if not set, compiles attributes dynamically from a DataRow and caches them in iterator
-            private RowAttribute[] attributes(DataRow row) {
-
-                if (itAttributes == null) {
-                    itAttributes = attributesFromDataRow(row);
-                }
-
-                return itAttributes;
-            }
-
             @Override
             public boolean hasNext() {
-                return drIt.hasNext();
+                return rows.hasNext();
             }
 
             @Override
             public Row next() {
-
-                DataRow row = drIt.next();
-                RowAttribute[] attributes = attributes(row);
-                return new DataRowRow(attributes, row);
+                DataRow row = rows.next();
+                return new DataRowRow(rowHeader, row);
             }
 
             @Override
@@ -64,22 +39,6 @@ public class JdbcRowReader implements RowReader {
                 throw new UnsupportedOperationException();
             }
         };
-    }
-
-    RowAttribute[] attributesFromDataRow(DataRow row) {
-
-        List<String> names = new ArrayList<>(row.keySet());
-
-        // ensure predictable order on each run...
-        Collections.sort(names);
-
-        RowAttribute[] attributes = new RowAttribute[row.size()];
-        for (int i = 0; i < attributes.length; i++) {
-            String name = names.get(i);
-            attributes[i] = new BaseRowAttribute(Object.class, name, ASTDbPath.DB_PREFIX + name, i);
-        }
-
-        return attributes;
     }
 
     @Override
