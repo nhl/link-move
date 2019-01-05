280f16f8ec19b7954704db62ef93c045230895b2
Andrus Adamchik <andrus@objectstyle.com>
2019-01-05 06:51:47 +0300

    DataFrame based micro-batches #161
    * adding 'read' to Index, as Index is used as a context for joins

--- link-move-dataframe/src/main/java/com/nhl/link/move/df/Index.java
+++ link-move-dataframe/src/main/java/com/nhl/link/move/df/Index.java
@@ -63,6 +63,14 @@ public abstract class Index {
 
     public abstract Index compactIndex();
 
+    public Object read(Object[] row, String columnName) {
+        return position(columnName).read(row);
+    }
+
+    public Object read(Object[] row, int columnPos) {
+        return getPositions()[columnPos].read(row);
+    }
+
     public Index addNames(String... extraNames) {
         return Zipper.zipIndex(this, withNames(extraNames));
     }
