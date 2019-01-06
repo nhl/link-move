8ec79588a62f4474670fc336c29447636941ca35
Andrus Adamchik <andrus@objectstyle.com>
2019-01-06 15:47:54 +0300

    Switch segments data structures to DataFrame #164
    * DeleteTask

--- link-move/src/main/java/com/nhl/link/move/runtime/task/delete/DeleteTask.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/task/delete/DeleteTask.java
@@ -3,6 +3,9 @@ package com.nhl.link.move.runtime.task.delete;
 import com.nhl.link.move.Execution;
 import com.nhl.link.move.batch.BatchProcessor;
 import com.nhl.link.move.batch.BatchRunner;
+import com.nhl.link.move.df.DataFrame;
+import com.nhl.link.move.df.DataRow;
+import com.nhl.link.move.df.Index;
 import com.nhl.link.move.runtime.cayenne.ITargetCayenneService;
 import com.nhl.link.move.runtime.task.BaseTask;
 import com.nhl.link.move.runtime.token.ITokenManager;
@@ -16,64 +19,70 @@ import java.util.Map;
 
 /**
  * A task that allows to delete target objects not present in the source.
- * 
+ *
  * @since 1.3
  */
 public class DeleteTask<T extends DataObject> extends BaseTask {
 
-	String extractorName;
-	int batchSize;
-	Class<T> type;
-	Expression targetFilter;
+    String extractorName;
+    int batchSize;
+    Class<T> type;
+    Expression targetFilter;
 
-	private DeleteSegmentProcessor<T> processor;
-	private ITargetCayenneService targetCayenneService;
+    private DeleteSegmentProcessor<T> processor;
+    private ITargetCayenneService targetCayenneService;
 
-	public DeleteTask(String extractorName, int batchSize, Class<T> type, Expression targetFilter,
-			ITargetCayenneService targetCayenneService, ITokenManager tokenManager, DeleteSegmentProcessor<T> processor) {
+    public DeleteTask(
+            String extractorName,
+            int batchSize,
+            Class<T> type,
+            Expression targetFilter,
+            ITargetCayenneService targetCayenneService,
+            ITokenManager tokenManager,
+            DeleteSegmentProcessor<T> processor) {
 
-		super(tokenManager);
+        super(tokenManager);
 
-		this.extractorName = extractorName;
-		this.batchSize = batchSize;
-		this.type = type;
-		this.targetFilter = targetFilter;
-		this.targetCayenneService = targetCayenneService;
-		this.processor = processor;
-	}
+        this.extractorName = extractorName;
+        this.batchSize = batchSize;
+        this.type = type;
+        this.targetFilter = targetFilter;
+        this.targetCayenneService = targetCayenneService;
+        this.processor = processor;
+    }
 
-	@Override
-	public Execution run(Map<String, ?> params) {
+    @Override
+    public Execution run(Map<String, ?> params) {
 
-		try (Execution execution = new Execution("DeleteTask:" + extractorName, params);) {
+        try (Execution execution = new Execution("DeleteTask:" + extractorName, params);) {
 
-			BatchProcessor<T> batchProcessor = createBatchProcessor(execution);
-			ResultIterator<T> data = createTargetSelect();
+            BatchProcessor<T> batchProcessor = createBatchProcessor(execution);
 
-			try {
-				BatchRunner.create(batchProcessor).withBatchSize(batchSize).run(data);
-			} finally {
-				// TODO: ResultIterator should be made Closeable in Cayenne,
-				// then we can use try-with-resources.
-				data.close();
-			}
+            try (ResultIterator<T> data = createTargetSelect()) {
+                BatchRunner.create(batchProcessor).withBatchSize(batchSize).run(data);
+            }
 
-			return execution;
-		}
-	}
+            return execution;
+        }
+    }
 
-	protected ResultIterator<T> createTargetSelect() {
-		ObjectSelect<T> query = ObjectSelect.query(type).where(targetFilter);
-		return targetCayenneService.newContext().iterator(query);
-	}
+    protected ResultIterator<T> createTargetSelect() {
+        ObjectSelect<T> query = ObjectSelect.query(type).where(targetFilter);
+        return targetCayenneService.newContext().iterator(query);
+    }
 
-	protected BatchProcessor<T> createBatchProcessor(final Execution execution) {
-		return segment -> {
+    protected BatchProcessor<T> createBatchProcessor(Execution execution) {
 
-			// executing in the select context..
-			ObjectContext context = segment.get(0).getObjectContext();
-			processor.process(execution, new DeleteSegment<>(context, segment));
-		};
-	}
+        Index columns = Index.withNames(DeleteSegment.TARGET_COLUMN);
 
+        return rows -> {
+
+            // executing in the select context..
+            ObjectContext context = rows.get(0).getObjectContext();
+
+            // TODO: Guess we need something like a Series object next to the DataFrame to handle single column data
+            DataFrame df = DataFrame.create(columns, rows, t -> DataRow.row(t));
+            processor.process(execution, new DeleteSegment<T>(context, df));
+        };
+    }
 }
--- link-move/src/main/java/com/nhl/link/move/runtime/task/delete/MissingTargetsFilterStage.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/task/delete/MissingTargetsFilterStage.java
@@ -1,32 +1,19 @@
 package com.nhl.link.move.runtime.task.delete;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
+import com.nhl.link.move.df.DataFrame;
+
 import java.util.Set;
-
-import org.apache.cayenne.ObjectContext;
-
-import com.nhl.link.move.Execution;
 
 /**
  * @since 1.3
  */
 public class MissingTargetsFilterStage<T> {
 
-	public List<T> filterMissing(Execution parentExec, ObjectContext context, Map<Object, T> mappedTargets,
-			Set<Object> sourceKeys) {
+    public DataFrame filterMissing(
+            DataFrame mappedTargets,
+            Set<Object> sourceKeys) {
 
-		List<T> matched = new ArrayList<>();
-
-		for (Entry<Object, T> e : mappedTargets.entrySet()) {
-			if (!sourceKeys.contains(e.getKey())) {
-				matched.add(e.getValue());
-			}
-		}
-
-		return matched;
-	}
-
+        return mappedTargets.filter(
+                (c, r) -> !sourceKeys.contains(c.get(r, DeleteSegment.KEY_COLUMN)));
+    }
 }
