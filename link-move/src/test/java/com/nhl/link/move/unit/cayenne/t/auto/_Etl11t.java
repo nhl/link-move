cba638d7a9472a16d0f80fa8e86d32db9e6f8227
Andrus Adamchik <andrus@objectstyle.com>
2019-10-03 18:26:28 +0300

    upgrading to Cayenne 4.1.RC1 that is now being released

--- link-move/src/test/java/com/nhl/link/move/unit/cayenne/t/auto/_Etl11t.java
+++ link-move/src/test/java/com/nhl/link/move/unit/cayenne/t/auto/_Etl11t.java
@@ -1,6 +1,10 @@
 package com.nhl.link.move.unit.cayenne.t.auto;
 
-import org.apache.cayenne.CayenneDataObject;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.apache.cayenne.BaseDataObject;
 import org.apache.cayenne.exp.Property;
 
 /**
@@ -9,7 +13,7 @@ import org.apache.cayenne.exp.Property;
  * since it may be overwritten next time code is regenerated.
  * If you need to make any customizations, please use subclass.
  */
-public abstract class _Etl11t extends CayenneDataObject {
+public abstract class _Etl11t extends BaseDataObject {
 
     private static final long serialVersionUID = 1L; 
 
@@ -17,11 +21,66 @@ public abstract class _Etl11t extends CayenneDataObject {
 
     public static final Property<byte[]> BIN = Property.create("bin", byte[].class);
 
+    protected byte[] bin;
+
+
     public void setBin(byte[] bin) {
-        writeProperty("bin", bin);
+        beforePropertyWrite("bin", this.bin, bin);
+        this.bin = bin;
     }
+
     public byte[] getBin() {
-        return (byte[])readProperty("bin");
+        beforePropertyRead("bin");
+        return this.bin;
+    }
+
+    @Override
+    public Object readPropertyDirectly(String propName) {
+        if(propName == null) {
+            throw new IllegalArgumentException();
+        }
+
+        switch(propName) {
+            case "bin":
+                return this.bin;
+            default:
+                return super.readPropertyDirectly(propName);
+        }
+    }
+
+    @Override
+    public void writePropertyDirectly(String propName, Object val) {
+        if(propName == null) {
+            throw new IllegalArgumentException();
+        }
+
+        switch (propName) {
+            case "bin":
+                this.bin = (byte[])val;
+                break;
+            default:
+                super.writePropertyDirectly(propName, val);
+        }
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        writeSerialized(out);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        readSerialized(in);
+    }
+
+    @Override
+    protected void writeState(ObjectOutputStream out) throws IOException {
+        super.writeState(out);
+        out.writeObject(this.bin);
+    }
+
+    @Override
+    protected void readState(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        super.readState(in);
+        this.bin = (byte[])in.readObject();
     }
 
 }
