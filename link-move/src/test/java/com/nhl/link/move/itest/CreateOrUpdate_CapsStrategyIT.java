940334ea0e32c2de09fe864823bf44a2d46eabe6
Andrus Adamchik <andrus@objectstyle.com>
2022-10-24 12:53:52 +0200

    tests refactoring

--- link-move/src/test/java/com/nhl/link/move/itest/CreateOrUpdate_CapsStrategyIT.java
+++ link-move/src/test/java/com/nhl/link/move/itest/CreateOrUpdate_CapsStrategyIT.java
@@ -5,9 +5,31 @@ import com.nhl.link.move.LmTask;
 import com.nhl.link.move.runtime.task.ITaskService;
 import com.nhl.link.move.unit.LmIntegrationTest;
 import com.nhl.link.move.unit.cayenne.t.Etl12t;
+import com.nhl.link.move.unit.cayenne.t.Etl1t;
 import org.junit.jupiter.api.Test;
 
 public class CreateOrUpdate_CapsStrategyIT extends LmIntegrationTest {
+
+    @Test
+    public void test_CapsLower() {
+
+        LmTask task = lmRuntime.service(ITaskService.class)
+                .createOrUpdate(Etl1t.class)
+                .sourceExtractor("com/nhl/link/move/itest/etl1_to_etl1t_lower.xml")
+                .matchBy(Etl1t.NAME)
+                .task();
+
+        srcEtl1().insertColumns("name", "age")
+                .values("a", 3)
+                .values("b", null)
+                .exec();
+
+        Execution e1 = task.run();
+        assertExec(2, 2, 0, 0, e1);
+        etl1t().matcher().assertMatches(2);
+        etl1t().matcher().eq("name", "a").eq("age", 3).assertOneMatch();
+        etl1t().matcher().eq("name", "b").eq("age", null).assertOneMatch();
+    }
 
     @Test
     public void test_DefaultCaps_MixedCase() {
