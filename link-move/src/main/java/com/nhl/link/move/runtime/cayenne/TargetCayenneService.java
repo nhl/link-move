93ccf3df370e6d0b0406c69ec93486334274f9e5
Andrus Adamchik <andrus@objectstyle.com>
2022-10-31 18:20:26 +0100

    formatting, no change

--- link-move/src/main/java/com/nhl/link/move/runtime/cayenne/TargetCayenneService.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/cayenne/TargetCayenneService.java
@@ -13,46 +13,46 @@ import java.util.Optional;
 
 public class TargetCayenneService implements ITargetCayenneService {
 
-	private final ServerRuntime runtime;
+    private final ServerRuntime runtime;
 
-	public TargetCayenneService(ServerRuntime runtime) {
-		this.runtime = runtime;
-	}
+    public TargetCayenneService(ServerRuntime runtime) {
+        this.runtime = runtime;
+    }
 
-	@Override
-	public Map<String, DataSource> dataSources() {
+    @Override
+    public Map<String, DataSource> dataSources() {
 
-		Map<String, DataSource> dataSources = new HashMap<>();
+        Map<String, DataSource> dataSources = new HashMap<>();
 
-		for (DataNode n : runtime.getDataDomain().getDataNodes()) {
+        for (DataNode n : runtime.getDataDomain().getDataNodes()) {
 
-			// this method is used to seed a special kind of JdbcConnector.
-			// But note that the DataSource here is attached to the DataNode
-			// transaction, so reading source data will happen over the same
-			// connection as writing target data. Hopefully such multiplexing
-			// the connection works ok...
+            // this method is used to seed a special kind of JdbcConnector.
+            // But note that the DataSource here is attached to the DataNode
+            // transaction, so reading source data will happen over the same
+            // connection as writing target data. Hopefully such multiplexing
+            // the connection works ok...
 
-			dataSources.put(n.getName(), n.getDataSource());
-		}
+            dataSources.put(n.getName(), n.getDataSource());
+        }
 
-		return dataSources;
-	}
+        return dataSources;
+    }
 
-	@Override
-	public ObjectContext newContext() {
-		return runtime.newContext();
-	}
+    @Override
+    public ObjectContext newContext() {
+        return runtime.newContext();
+    }
 
-	@Override
-	public EntityResolver entityResolver() {
-		return runtime.getChannel().getEntityResolver();
-	}
+    @Override
+    public EntityResolver entityResolver() {
+        return runtime.getChannel().getEntityResolver();
+    }
 
-	@Override
-	public Optional<DbAdapter> dbAdapter(String nodeName) {
-		return runtime.getDataDomain().getDataNodes().stream()
-				.filter(node -> node.getName().equals(nodeName))
-				.map(DataNode::getAdapter)
-				.findFirst();
-	}
+    @Override
+    public Optional<DbAdapter> dbAdapter(String nodeName) {
+        return runtime.getDataDomain().getDataNodes().stream()
+                .filter(node -> node.getName().equals(nodeName))
+                .map(DataNode::getAdapter)
+                .findFirst();
+    }
 }
