bd4a32f9581ffa63cf705250c3196055b8819564
Andrus Adamchik <andrus@objectstyle.com>
2025-03-22 13:24:03 -0400

    Deprecate StageListener and TaskStageType #240
    enums implementing TaskStageType should actually stay undeprecated, only they interface methods are deprecated

--- link-move/src/main/java/com/nhl/link/move/runtime/task/common/TaskStageType.java
+++ link-move/src/main/java/com/nhl/link/move/runtime/task/common/TaskStageType.java
@@ -3,10 +3,15 @@ package com.nhl.link.move.runtime.task.common;
 import java.lang.annotation.Annotation;
 
 /**
+ * A tag interface for enums that list stages of various tasks.
+ *
  * @since 3.0.0
- * @deprecated the old style listeners were replaced with stage callbacks
  */
-@Deprecated(since = "3.0.0", forRemoval = true)
 public interface TaskStageType {
+
+    /**
+     * @deprecated the old style listeners were replaced with stage callbacks
+     */
+    @Deprecated(since = "3.0.0", forRemoval = true)
     Class<? extends Annotation> getLegacyAnnotation();
 }
